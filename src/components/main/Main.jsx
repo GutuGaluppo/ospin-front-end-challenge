f377ff08d0b04f9ea2a47af867ec55fd5507af58
Gutu Galuppo <galuppodev@gmail.com>
2022-02-02 14:07:27 +0100

    fix: url reflecting that the user is on the error page

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable spaced-comment */
 import React from 'react'
 import { Container } from 'semantic-ui-react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 
 import OspinSidebar from 'components/main/OspinSidebar'
 import Devices from 'components/devices/Devices'
@@ -24,7 +24,8 @@ const Main = () => (
           <Route path='/profile' component={Profile} />
           <Route path='/notifications' component={Notifications} />
           <Route path='/changelog' component={Changelog} />
-          <Route component={ErrorPage} />
+          <Route path='/error' component={ErrorPage} />
+          <Redirect to='/error' />
         </Switch>
       </Container>
 
