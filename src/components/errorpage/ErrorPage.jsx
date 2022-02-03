32910c6e407b7df9e770827dbea303e85b1734d6
Gutu Galuppo <galuppodev@gmail.com>
2022-02-03 12:17:53 +0100

    fix: changed error picture

--- src/components/errorpage/ErrorPage.jsx
+++ src/components/errorpage/ErrorPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 import { Container, Image } from 'semantic-ui-react'
-import pageNotFound from 'images/404illustration.png'
+import pageNotFound from 'images/500Illustration.png'
 
 const ErrorPage = () => (
   <Container className='error-container'>
-    <Image src={pageNotFound} alt='404 error picture' fluid />
+    <Image src={pageNotFound} alt='500 error picture' fluid />
     <h1>Sorry, Page Not Found</h1>
   </Container>
 )
